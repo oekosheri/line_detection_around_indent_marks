5d8ccf9e747a3b6d466583ffb06ca96566770514
hd242061 <shahrzadhadian@gmail.com>
2023-01-31 16:05:42 +0100

    commented run file

--- run_detect_line.sh
+++ run_detect_line.sh
@@ -7,7 +7,7 @@ conda activate tensor_new
 export CUDA_VISIBLE_DEVICES=""
 
 echo $CUDA_VISIBLE_DEVICES
-# run detect line image and model path need to change to your paths
+# run detect slip line: image and model path need to change to your paths
 python -W ignore ./detect_slip_lines.py  \
 --image_path $WORK/images_collective/leo1_1.png \
 --model_path ./indent_segmentation/sub_010.0012 \
